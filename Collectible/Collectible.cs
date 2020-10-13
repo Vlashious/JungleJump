01b08583b3471b7228312164e203eac83e6913aa
Uładzimir Śniežka <flashro561@gmail.com>
2020-10-13 14:48:46 +0300

    collectibles scene added

---
+++ Collectible/Collectible.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class Collectible : Area2D
+{
+    [Signal] private delegate void Pickup();
+    private readonly Dictionary<string, string> textures = new Dictionary<string, string>()
+    {
+        {"cherry", "res://assets/sprites/cherry.png"},
+        {"gem", "res://assets/sprites/gem.png"}
+    };
+    public void Init(string type, Vector2 pos)
+    {
+        GetNode<Sprite>("Sprite").Texture = GD.Load(textures[type]) as Texture;
+        Position = pos;
+        GetNode<Area2D>("Collectible").Connect("body_entered", this, "OnBodyEntered");
+    }
+    private void OnBodyEntered()
+    {
+        EmitSignal("Pickup");
+        QueueFree();
+    }
+}
